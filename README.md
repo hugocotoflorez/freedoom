fbf5dc272cde76009cd8a40a71d2f3d621b4e7c7
Hugo Coto Florez <hugocoto100305@gmail.com>
2025-04-15 01:00:01 +0200

    adding src

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # freedoom
+
+## Startpoint
+[https://github.com/hugocotoflorez/grua2](https://github.com/hugocotoflorez/grua2) (oop version)
---
+++ frog.c
@@ -0,0 +1,25 @@
+#define FROG_IMPLEMENTATION
+#include "thirdparty/frog/frog.h"
+
+
+#define FILENAME "freedoom"
+#define FLAGS "-Wall", "-Wextra", "-Wno-unused-parameter"
+#define LIBS "-lglad", "-lglfw"
+#define SRC "src/main.cpp"
+#define CC "g++"
+
+int
+main(int argc, char *argv[])
+{
+        frog_rebuild_itself(argc, argv);
+        frog_cmd_wait(CC, SRC, LIBS, FLAGS, "-o", FILENAME);
+
+        //UNREACHABLE("Just do not execute");
+
+        frog_cmd_wait("sudo", "systemctl", "stop", "keyd");
+        frog_cmd_wait("./" FILENAME);
+        frog_cmd_wait("sudo", "systemctl", "start", "keyd");
+        frog_cmd_wait("rm", FILENAME);
+
+        return 0;
+}
