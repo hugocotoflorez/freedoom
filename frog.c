a9d45e6626d3aae0ab0b8948aa739b805120b6c8
Hugo Coto Florez <hugocoto100305@gmail.com>
2025-04-18 01:13:34 +0200

    removing old boilerplate

--- frog.c
+++ frog.c
@@ -1,25 +1,22 @@
 #define FROG_IMPLEMENTATION
 #include "thirdparty/frog/frog.h"
 
-
 #define FILENAME "freedoom"
 #define FLAGS "-Wall", "-Wextra", "-Wno-unused-parameter"
 #define LIBS "-lglad", "-lglfw"
 #define SRC "src/main.cpp"
 #define CC "g++"
 
-int
-main(int argc, char *argv[])
-{
-        frog_rebuild_itself(argc, argv);
-        frog_cmd_wait(CC, SRC, LIBS, FLAGS, "-o", FILENAME);
+int main(int argc, char *argv[]) {
+  frog_rebuild_itself(argc, argv);
+  frog_cmd_wait(CC, SRC, LIBS, FLAGS, "-o", FILENAME);
 
-        //UNREACHABLE("Just do not execute");
+  // UNREACHABLE("Just do not execute");
 
-        frog_cmd_wait("sudo", "systemctl", "stop", "keyd");
-        frog_cmd_wait("./" FILENAME);
-        frog_cmd_wait("sudo", "systemctl", "start", "keyd");
-        frog_cmd_wait("rm", FILENAME);
+  frog_cmd_wait("sudo", "systemctl", "stop", "keyd");
+  frog_cmd_wait("./" FILENAME);
+  frog_cmd_wait("sudo", "systemctl", "start", "keyd");
+  frog_cmd_wait("rm", FILENAME);
 
-        return 0;
+  return 0;
 }
