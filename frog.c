753df927bc65ace57aea387afdf23bad80f4c26f
Hugo Coto Florez <hugocoto100305@gmail.com>
2025-05-04 18:25:46 +0200

    Settings file at runtime using vshcfp

--- frog.c
+++ frog.c
@@ -12,18 +12,19 @@ main(int argc, char *argv[])
         frog_rebuild_itself(argc, argv);
 
         frog_cmd_filtered_foreach("thirdparty/load_obj", ".*cpp", CC, FLAGS, "-c");
+        frog_cmd_filtered_foreach("thirdparty/vshcfp/src", ".*c$", "gcc" , FLAGS, "-c");
         frog_cmd_filtered_foreach("src", ".*cpp", CC, FLAGS, "-c");
         frog_makedir("objs");
         frog_shell_cmd("mv *.o objs")
         frog_shell_cmd("g++ objs/* -lglad -lglfw -ggdb -o " FILENAME);
+        frog_cmd_wait("rm", "objs", "-r");
 
-        // UNREACHABLE("Just do not execute");
+        UNREACHABLE("Just do not execute");
 
         frog_cmd_wait("sudo", "systemctl", "stop", "keyd");
         frog_cmd_wait("./" FILENAME);
         frog_cmd_wait("sudo", "systemctl", "start", "keyd");
         frog_cmd_wait("rm", FILENAME);
-        frog_cmd_wait("rm", "objs", "-r");
 
         return 0;
 }
--- src/settings.h
+++ src/settings.h
@@ -1,23 +1,57 @@
 #ifndef SETTINGS_H_
 #define SETTINGS_H_
 
+/* This code is quite weird as I migrate the original settings.h
+ * that was a file of defines to vshcfp and this is the easiest
+ * way to do that I think */
+
 
 /* Mouse sensibility */
-#define MOUSE_SENS_X 2.0f
-#define MOUSE_SENS_Y 2.0f
-#define MOUSE_FIRST_PERSON_SENS_X 2.0f
-#define MOUSE_FIRST_PERSON_SENS_Y 4.0f
-#define MOUSE_REDUCTION 0.001f
+static float MOUSE_SENS_X = 2.0f;
+static float MOUSE_SENS_Y = 2.0f;
+static float MOUSE_FIRST_PERSON_SENS_X = 2.0f;
+static float MOUSE_FIRST_PERSON_SENS_Y = 4.0f;
+static float MOUSE_REDUCTION = 0.001f;
 
-/* Show fps if SHOW_FPS is defined and not 0 */
-#define SHOW_FPS 1
+static bool SHOW_FPS = true;
 
-/* Enable VSync. FPS limited to screen refresh rate
- * (0: disable, 1: enable, undef: default) */
-#define VSYNC 1
+/* Enable VSync. FPS limited to screen refresh rate */
+static bool VSYNC = true;
 
-#define show_collision_point 0
-#define draw_collision_sphere 0
+static bool show_collision_point = false;
+static bool draw_collision_sphere = false;
+
+extern "C" {
+/* Literally fuck cpp */
+#include "../thirdparty/vshcfp/include/vshcfp.h"
+}
+
+#include <cstdio>
+
+#include "cstdlib"
+#include "cstring"
+
+__attribute__((constructor)) static void
+__init__()
+{
+        HcfOpts opts;
+        char *s;
+
+        opts = hcf_load("settings.hcf");
+
+        if ((s = hcf_get(opts, "mouse", "sens_x"))) MOUSE_SENS_X = atof(s);
+        if ((s = hcf_get(opts, "mouse", "sens_y"))) MOUSE_SENS_Y = atof(s);
+        if ((s = hcf_get(opts, "mouse", "sens_x"))) MOUSE_FIRST_PERSON_SENS_X = atof(s);
+        if ((s = hcf_get(opts, "mouse", "sens_y"))) MOUSE_FIRST_PERSON_SENS_Y = atof(s);
+        if ((s = hcf_get(opts, "mouse", "reduction"))) MOUSE_REDUCTION = atof(s);
+        if ((s = hcf_get(opts, "options", "show_fps"))) SHOW_FPS = 0 == strcmp(s, "on");
+        if ((s = hcf_get(opts, "options", "vsync"))) VSYNC = 0 == strcmp(s, "on");
+
+        if ((s = hcf_get(opts, "dev", "show_collision_point"))) show_collision_point = 0 == strcmp(s, "on");
+        if ((s = hcf_get(opts, "dev", "draw_collision_sphere"))) draw_collision_sphere = 0 == strcmp(s, "on");
+
+        hcf_destroy(&opts);
+}
 
 
 #endif // SETTINGS_H_
